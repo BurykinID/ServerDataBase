e4b7850c712e9a2699289614015cee7dd78c2e83
Burykin Ivan <burykin.ivan2016@gmail.com>
2020-04-03 18:57:48 +0300

    upd file filter

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.File;
+import com.example.demo.entity.User;
 import com.example.demo.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -22,10 +27,29 @@ public class MainController {
     }
 
     @GetMapping(value = "/listFile")
-    public String personList(Map<String, Object> model) {
+    public String personList( @AuthenticationPrincipal User user,
+                              Map<String, Object> model) {
 
-        Iterable<File> files = fileRepository.findAll();
-        model.put("files", files);
+
+        List<File> files = fileRepository.findAll();
+
+        ArrayList<File> accessFiles = new ArrayList<>();
+
+        String username = user.getUsername();
+
+        for (File file: files) {
+
+            ArrayList<String> accessList = file.getAccessList();
+            for (String accessUsername: accessList) {
+                if (username.equals(accessUsername)) {
+                    accessFiles.add(file);
+                    break;
+                }
+            }
+
+        }
+
+        model.put("files", accessFiles);
 
         return "listFile";
     }
